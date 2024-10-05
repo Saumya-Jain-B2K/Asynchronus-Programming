6f6e5baecb0f46e59cab4cea55dfb0ad37f0dda2
“Saumya-Jain-B2K” <saumyajain2020@gmail.com>
2024-10-05 22:17:00 +0530

    done

--- day 6/asyncawait.js
+++ day 6/asyncawait.js
@@ -1,15 +1,14 @@
-console.log("Hello");
-
 async function fetchData() {
     try{
-        const data = await new Promise((resolve, reject) => {
+        const data = await Promise((resolve, reject) => {
             setTimeout(() => {
                 reject('Error fetching data');
-            })
-        }, 1000);
-    };
-    catch (error) {
-        console.error('Caught an error:', error);
-        }
+            }, 1000);
+        });
         console.log(data);
-}    
+    } catch (error) {
+        console.error('Caught an error:', error);
+    }
+}
+
+fetchData();
